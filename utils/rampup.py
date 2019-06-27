7c78d05a6763c61c619e0b893fefcd7f39775e58
Daniel Rohr <rohrdr@gmail.com>
2019-06-27 11:33:55 +0200

    merging Emily and Emil

---
+++ utils/rampup.py
@@ -0,0 +1,50 @@
+def rampup(cfg, model_path=None, use_joystick=False, use_chaos=False):
+    """
+    Construct a working robotic vehicle from many parts.
+    Each part runs as a job in the Vehicle loop, calling either
+    it's run or run_threaded method depending on the constructor flag `threaded`.
+    All parts are updated one after another at the framerate given in
+    cfg.DRIVE_LOOP_HZ assuming each part finishes processing in a timely manner.
+    Parts may have named outputs and inputs. The framework handles passing named outputs
+    to parts requesting the same named input.
+    """
+
+    V = dk.vehicle.Vehicle()
+
+    clock = Timestamp()
+    V.add(clock, outputs=['timestamp'])
+
+    ramper = Rampup(cfg.DRIVE_LOOP_HZ, cfg.MAX_LOOPS)
+    V.add(ramper, outputs=['angle', 'throttle'])
+
+    steering_controller = PCA9685(cfg.STEERING_CHANNEL)
+    steering = PWMSteering(controller=steering_controller,
+                           left_pulse=cfg.STEERING_LEFT_PWM,
+                           right_pulse=cfg.STEERING_RIGHT_PWM)
+
+    throttle_controller = PCA9685(cfg.THROTTLE_CHANNEL)
+    throttle = PWMThrottle(controller=throttle_controller,
+                           max_pulse=cfg.THROTTLE_FORWARD_PWM,
+                           zero_pulse=cfg.THROTTLE_STOPPED_PWM,
+                           min_pulse=cfg.THROTTLE_REVERSE_PWM)
+
+    V.add(steering, inputs=['angle'])
+    V.add(throttle, inputs=['throttle'])
+
+    # add tub to save data
+    inputs = ['angle', 'throttle']
+    types = ['float', 'float']
+
+    # multiple tubs
+    # th = TubHandler(path=cfg.DATA_PATH)
+    # tub = th.new_tub_writer(inputs=inputs, types=types)
+
+    # single tub
+    tub = TubWriter(path=cfg.TUB_PATH, inputs=inputs, types=types)
+    V.add(tub, inputs=inputs)
+
+    # run the vehicle
+    for i in range(cfg.NUM_DRIVES):
+        V.start(rate_hz=cfg.DRIVE_LOOP_HZ, max_loop_count=cfg.MAX_LOOPS)
+        time.sleep(1)
+        ramper.reset()
